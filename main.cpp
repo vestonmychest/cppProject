27861ca09e66543d3e9b2c7bfc681e1123dc7b27
840 G5 <sampohulkko@gmail.com>
2025-02-25 14:50:37 +0200

    button

--- main.cpp
+++ main.cpp
@@ -24,132 +24,64 @@
 #define UART_RX_PIN 5
 #endif
 
-#define BAUD_RATE 9600
-#define STOP_BITS 1 // for simulator
-//#define STOP_BITS 2 // for real system
 
 #define USE_MQTT
 
-void messageArrived(MQTT::MessageData &md) {
-    MQTT::Message &message = md.message;
-
-    printf("Message arrived: qos %d, retained %d, dup %d, packetid %d\n",
-           message.qos, message.retained, message.dup, message.id);
-    printf("Payload %s\n", (char *) message.payload);
-}
-
-static const char *topic = "test-topic";
 
 
+class StepperMotor{
+private:
+    uint8_t pin1, pin2 ,pin3, pin4;
+    public:
+    StepperMotor(uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4):pin1(pin1), pin2(pin2), pin3(pin3), pin4(pin4) {}
+
+    void initialize() {
+            gpio_init(pin1);
+            gpio_set_dir(pin1, GPIO_OUT);
+            gpio_init(pin2);
+            gpio_set_dir(pin2, GPIO_OUT);
+            gpio_init(pin3);
+            gpio_set_dir(pin3, GPIO_OUT);
+            gpio_init(pin4);
+            gpio_set_dir(pin4, GPIO_OUT);
+
+
+    }
+
+};
+class Button {
+private:
+    uint8_t pin;
+
+    public :
+        Button(uint pin, bool pullup = true, bool invert = false) : pin(pin) {}
+
+    void initialize() {
+        gpio_init(pin);
+        gpio_set_dir(pin, GPIO_IN);
+        gpio_pull_up(pin); // Käytetään sisäistä pull-up vastusta
+    }
+
+    bool ispressed() {
+        return gpio_get(pin) == 0;
+    }
+
+};
 int main() {
-    const uint led_pin = 22;
-    const uint button = 9;
 
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
-
-    gpio_init(button);
-    gpio_set_dir(button, GPIO_IN);
-    gpio_pull_up(button);
-
-    // Initialize chosen serial port
     stdio_init_all();
+    Button button(8);
+    button.initialize(); // Alustetaan painike
+
+    while (true) {
+        if (button.ispressed()) {
+            printf("pressed\n");
+        }
+
+    }
+
+
+
 
     printf("\nBoot\n");
-
-#ifdef USE_MQTT
-    //IPStack ipstack("SSID", "PASSWORD"); // example
-    //IPStack ipstack("KME662", "SmartIot"); // example
-    IPStack ipstack("SmartIotMQTT", "SmartIot"); // example
-    auto client = MQTT::Client<IPStack, Countdown>(ipstack);
-
-    int rc = ipstack.connect("192.168.1.10", 1883);
-    if (rc != 1) {
-        printf("rc from TCP connect is %d\n", rc);
-    }
-
-    printf("MQTT connecting\n");
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = 3;
-    data.clientID.cstring = (char *) "PicoW-sample";
-    rc = client.connect(data);
-    if (rc != 0) {
-        printf("rc from MQTT connect is %d\n", rc);
-        while (true) {
-            tight_loop_contents();
-        }
-    }
-    printf("MQTT connected\n");
-
-    // We subscribe QoS2. Messages sent with lower QoS will be delivered using the QoS they were sent with
-    rc = client.subscribe(topic, MQTT::QOS2, messageArrived);
-    if (rc != 0) {
-        printf("rc from MQTT subscribe is %d\n", rc);
-    }
-    printf("MQTT subscribed\n");
-
-    auto mqtt_send = make_timeout_time_ms(2000);
-    int mqtt_qos = 0;
-    int msg_count = 0;
-#endif
-
-#ifdef USE_MQTT
-    if (time_reached(mqtt_send)) {
-        mqtt_send = delayed_by_ms(mqtt_send, 2000);
-        if (!client.isConnected()) {
-            printf("Not connected...\n");
-            rc = client.connect(data);
-            if (rc != 0) {
-                printf("rc from MQTT connect is %d\n", rc);
-            }
-        }
-        char buf[100];
-        int rc = 0;
-        MQTT::Message message;
-        message.retained = false;
-        message.dup = false;
-        message.payload = (void *) buf;
-        switch (mqtt_qos) {
-            case 0:
-                // Send and receive QoS 0 message
-                sprintf(buf, "Msg nr: %d QoS 0 message", ++msg_count);
-                printf("%s\n", buf);
-                message.qos = MQTT::QOS0;
-                message.payloadlen = strlen(buf) + 1;
-                rc = client.publish(topic, message);
-                printf("Publish rc=%d\n", rc);
-                ++mqtt_qos;
-                break;
-            case 1:
-                // Send and receive QoS 1 message
-                sprintf(buf, "Msg nr: %d QoS 1 message", ++msg_count);
-                printf("%s\n", buf);
-                message.qos = MQTT::QOS1;
-                message.payloadlen = strlen(buf) + 1;
-                rc = client.publish(topic, message);
-                printf("Publish rc=%d\n", rc);
-                ++mqtt_qos;
-                break;
-#if MQTTCLIENT_QOS2
-                    case 2:
-                        // Send and receive QoS 2 message
-                        sprintf(buf, "Msg nr: %d QoS 2 message", ++msg_count);
-                        printf("%s\n", buf);
-                        message.qos = MQTT::QOS2;
-                        message.payloadlen = strlen(buf) + 1;
-                        rc = client.publish(topic, message);
-                        printf("Publish rc=%d\n", rc);
-                        ++mqtt_qos;
-                        break;
-#endif
-            default:
-                mqtt_qos = 0;
-                break;
-        }
-    }
-
-    cyw43_arch_poll(); // obsolete? - see below
-    client.yield(100); // socket that client uses calls cyw43_arch_poll()
-#endif}
 }
